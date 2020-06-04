03d8170fee4a0d20d50b786a60278c99d21f4944
shahbaz-pycread <alam.shahbaz01@gmail.com>
2020-06-04 21:02:50 +0530

    add script.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    var userName = prompt("May I know your Name: ");
+    var userNameElement = document.getElementById("name");
+    userNameElement.innerText = "Hello " + userName;
+}
